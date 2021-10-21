56812593c3bd45ddc3f5f58ca9dc85f3774f51bf
SebastianLopezOsorno <sebastianlopezosorno@pascualbravo.edu.co>
2021-10-20 21:34:47 -0500

    Update index.php

--- Mistakes/index.php
+++ Mistakes/index.php
@@ -19,14 +19,14 @@
             <a href="/es/Home">The Company Dream</a>
         </header>
         <div class="banner">
-        <img class="fondo" src="/mistakes/fondo.jpg">
+        <img class="fondo" src="/Mistakes/fondo.jpg">
             <div class="textBox">
-                <h2>Error <?php echo($_GET['error']) ?> <img src="/mistakes/error.png"></h2>
+                <h2>Error <?php echo($_GET['error']) ?> <img src="/Mistakes/error.png"></h2>
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged</p>
                 <a href="/es/Home">Regresar</a>
             </div>
             <div class="FondoBx">
-                <img class="fondo" src="/mistakes/fondo.jpg">
+                <img class="fondo" src="/Mistakes/fondo.jpg">
             </div>
         </div>
     </body>
