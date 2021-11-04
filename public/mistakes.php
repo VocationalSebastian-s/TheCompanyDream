3ee484346d069e8bde499211f72e1e625e51138a
Sebastian <sebastianlopezosorno@pascualbravo.edu.co>
2021-11-03 21:16:04 -0500

    main

---
+++ public/mistakes.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <!---Meta Etiquetas-->
+        <?php include('../assets/utils/index.html') ?>
+        <!---Stylesheet-->
+        <style>
+            <?php include('../assets/fonts/style.css') ?>
+            <?php include('../assets/utils/style.css') ?>
+            <?php include('../assets/components/mistakes/style.css') ?>
+        </style>
+    </head>
+    <body>
+        <?php include('../assets/components/mistakes/index.html') ?>
+    </body>
+</html>
