c3392cee95dde3fc8d6b8221c585a7c8ec59ddb2
Sebastian <sebastianlopezosorno@pascualbravo.edu.co>
2021-11-03 23:43:04 -0500

    main

--- public/es/login/index.php
+++ public/es/login/index.php
@@ -1,5 +1,4 @@
 <?php
-    echo $_SERVER['DOCUMENT_ROOT'];
     require '../../../server/connection/conexion.php';
 
     $message = '';
--- public/es/main/index.php
+++ public/es/main/index.php
@@ -1,27 +1,25 @@
 <?php
-	echo $_SERVER['DOCUMENT_ROOT'];
-	require '../../../server/secure/seguridad.php';
 	require '../../../server/connection/conexion.php';
+	require '../../../server/secure/seguridad.php';
 ?>
 <?php if (!empty($user)): ?>
 <!DOCTYPE html>
 <html>
 	<head>
 		<!-- Bootstrap CSS -->
-		<link rel="stylesheet" href="../assets/components/main/src/js/bootstrap.min.css">
+		<link rel="stylesheet" href="../assets/components/main/src/css/bootstrap.min.css">
 		<!----css3---->
 		<link rel="stylesheet" href="../assets/components/main/src/css/custom.css">
 		<!-- SLIDER REVOLUTION 4.x CSS SETTINGS -->
 	
 		<link rel="preconnect" href="https://fonts.googleapis.com">
 		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-		<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet">
 
 		<!--google material icon-->
 		<link href="https://fonts.googleapis.com/css2?family=Material+Icons" rel="stylesheet">
 	</head>
 	<body>
-		<?php include('../../assets/components/main/index.html') ?>
+		<?php include('../assets/components/main/index.html') ?>
 	</body>
 </html>
 <?php else: ?>
