e258a54217088d8933979c3edbc4f6770bbe1b23
Sebastian <sebastianlopezosorno@pascualbravo.edu.co>
2021-11-08 16:23:58 -0500

    MAIN

--- public/es/login/index.php
+++ public/es/login/index.php
@@ -76,78 +76,80 @@
                 if ($_POST['password'] == $_POST['passwordcheck']) {
                     if (verifypassword($_POST['password'])==1) {
                         if ($_POST['terms']=="yes") {
-                            $sql = 'SELECT email FROM usuarios WHERE email = :email';
-                            $datos = $conexion->prepare($sql);
-                            $datos->bindParam(':email', $_POST['email']);
-                            if ($datos->execute()) {
-                                try {
-                                    $usuarios = $datos->fetch(PDO::FETCH_ASSOC);
-                                    if (is_array($usuarios)) {
-                                        if ($usuarios['email'] == $_POST['email']) {
-                                            $repeated = true;
+                            if ($_POST['type']<2 && $_POST['type']>-1) {
+                                $sql = 'SELECT email FROM usuarios WHERE email = :email';
+                                $datos = $conexion->prepare($sql);
+                                $datos->bindParam(':email', $_POST['email']);
+                                if ($datos->execute()) {
+                                    try {
+                                        $usuarios = $datos->fetch(PDO::FETCH_ASSOC);
+                                        if (is_array($usuarios)) {
+                                            if ($usuarios['email'] == $_POST['email']) {
+                                                $repeated = true;
+                                            } else {
+                                                $repeated = false;
+                                            }
                                         } else {
-                                            $repeated = false;
+                                            $repeated=false;
                                         }
-                                    }else{
-                                        $repeated=false;
+                                    } catch (Exception) {
+                                        $GLOBALS['icon'] = 'error';
+                                        $GLOBALS['title'] = 'Error';
+                                        $GLOBALS['text'] = 'No se pudo verificar la existencia de la cuenta';
                                     }
-                                } catch (Exception) {
+                                } else {
                                     $GLOBALS['icon'] = 'error';
                                     $GLOBALS['title'] = 'Error';
                                     $GLOBALS['text'] = 'No se pudo verificar la existencia de la cuenta';
                                 }
+                                if ($repeated == false) {
+                                    $sql = 'INSERT INTO usuarios (id,name,lastname,direction,telephone,email,password,type,img,role,company) values (:id,:name,:lastname,:direction,:telephone,:email,:password,:type,:img,:role,:company)';
+                                    $datos = $conexion->prepare($sql);
+                                    /*Variables */
+                                    $id=date("mdHis");
+                                    $temp=name($_POST['name']);
+                                    $name=$temp[0];
+                                    $lastname=$temp[1];
+                                    $direction='';
+                                    $telephone='';
+                                    $email=$_POST['email'];
+                                    $password=password_hash($_POST['password'], PASSWORD_BCRYPT);/*Cifrar contraseña en hash BCRYPT */
+                                    $type=$_POST['type'];
+                                    $img='user.png';
+                                    $role='';
+                                    $company='';
+                                    /*Pasar Parametros al sql */
+                                    $datos->bindParam(':id', $id);
+                                    $datos->bindParam(':name', $name);
+                                    $datos->bindParam(':lastname', $lastname);
+                                    $datos->bindParam(':direction', $direction);
+                                    $datos->bindParam(':telephone', $telephone);
+                                    $datos->bindParam(':email', $email);
+                                    $datos->bindParam(':password', $password);
+                                    $datos->bindParam(':type', $type);
+                                    $datos->bindParam(':img', $img);
+                                    $datos->bindParam(':role', $role);
+                                    $datos->bindParam(':company', $company);
+                                
+                                    if ($datos->execute()) {
+                                        $GLOBALS['icon'] = 'success';
+                                        $GLOBALS['title'] = 'Éxito';
+                                        $GLOBALS['text'] = 'La cuenta ha sido creada con exito';
+                                        signin($conexion);
+                                    } else {
+                                        $GLOBALS['icon'] = 'error';
+                                        $GLOBALS['title'] = 'Error';
+                                        $GLOBALS['text'] = 'La cuenta no se pudo crear';
+                                    }
+                                } elseif ($repeated == true) {
+                                    $GLOBALS['icon'] = 'error';
+                                    $GLOBALS['title'] = 'Error';
+                                    $GLOBALS['text'] = 'El correo ' . $_POST['email'] . ' ya existe';
+                                }
                             }else{
                                 $GLOBALS['icon'] = 'error';
                                 $GLOBALS['title'] = 'Error';
-                                $GLOBALS['text'] = 'No se pudo verificar la existencia de la cuenta';
-                            }
-                            if ($repeated == false) {
-                                $sql = 'INSERT INTO usuarios (id,name,lastname,direction,telephone,email,password,type,img,role,company) values (:id,:name,:lastname,:direction,:telephone,:email,:password,:type,:img,:role,:company)';
-                                $datos = $conexion->prepare($sql);
-                                /*Variables */
-                                $id=date("mdHis");
-                                $temp=name($_POST['name']);
-                                $name=$temp[0];
-                                $lastname=$temp[1];
-                                $direction='';
-                                $telephone='';
-                                $email=$_POST['email'];
-                                $password=password_hash($_POST['password'], PASSWORD_BCRYPT);/*Cifrar contraseña en hash BCRYPT */
-                                if($_POST['type']<2 && $_POST['type']>-1){
-                                    $type=$_POST['type'];
-                                }else{
-                                    $type=0;
-                                }
-                                $img='user.png';
-                                $role='';
-                                $company='';
-                                /*Pasar Parametros al sql */
-                                $datos->bindParam(':id', $id);
-                                $datos->bindParam(':name', $name);
-                                $datos->bindParam(':lastname', $lastname);
-                                $datos->bindParam(':direction', $direction);
-                                $datos->bindParam(':telephone', $telephone);
-                                $datos->bindParam(':email', $email);
-                                $datos->bindParam(':password', $password); 
-                                $datos->bindParam(':type', $type);
-                                $datos->bindParam(':img', $img);
-                                $datos->bindParam(':role', $role);
-                                $datos->bindParam(':company', $company);
-                                
-                                if ($datos->execute()) {
-                                    $GLOBALS['icon'] = 'success';
-                                    $GLOBALS['title'] = 'Éxito';
-                                    $GLOBALS['text'] = 'La cuenta ha sido creada con exito';
-                                    signin($conexion);
-                                }else{
-                                    $GLOBALS['icon'] = 'error';
-                                    $GLOBALS['title'] = 'Error';
-                                    $GLOBALS['text'] = 'La cuenta no se pudo crear';
-                                }
-                            } elseif ($repeated == true) {
-                                $GLOBALS['icon'] = 'error';
-                                $GLOBALS['title'] = 'Error';
-                                $GLOBALS['text'] = 'El correo ' . $_POST['email'] . ' ya existe';
+                                $GLOBALS['text'] = 'El tipo de Usuario no es correcto';
                             }
                         }else{
                             $GLOBALS['icon'] = 'error';
