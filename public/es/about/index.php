bf75190ef617647fc8b0268304e747e5d695d553
Sebastián López Osorno <68956039+SebastianLopezOsorno@users.noreply.github.com>
2021-11-10 11:01:13 -0500

    Create index.php

---
+++ public/es/about/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <!---Meta Etiquetas-->
+        <?php include('../../assets/utils/index.html') ?>
+        <!---Stylesheet-->
+        <style type="text/css">
+            <?php include('../../assets/fonts/style.css') ?>
+            <?php include('../../assets/utils/style.css') ?>
+        </style>
+        <link rel="stylesheet" href="../assets/utils/header/style.css">
+        <link rel="stylesheet" href="../assets/components/about/style.css">
+    </head>
+    <body>
+        <!---Header-->
+        <?php include('../assets/utils/header/index.html') ?>
+        <?php include('../assets/components/about/index.html') ?>
+    </body>
+</html>
